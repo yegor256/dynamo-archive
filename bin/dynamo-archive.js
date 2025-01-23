1e38cfc46483f9bda0f90b58f23020324454127c
Yegor Bugayenko <yegor256@gmail.com>
2025-01-23 12:16:23 +0300

    year set to 2025

--- bin/dynamo-archive.js
+++ bin/dynamo-archive.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /**
- * Copyright 2013-2023 Yegor Bugayenko
+ * Copyright 2013-2025 Yegor Bugayenko
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
